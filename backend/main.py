2f6a6b4cb32ce575f661e22787b61e478abfcc1d
ashen-v2 <ashen.te0217@gmail.com>
2026-03-04 20:15:13 +0530

    added CORS

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import Depends, FastAPI
 from fastapi.concurrency import asynccontextmanager
 from db.session import engine, get_session
+from fastapi.middleware.cors import CORSMiddleware
 from routes import user_routes
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,6 +14,15 @@ async def lifespan(app: FastAPI):
         print(f"Error connecting to PostgreSQL database: {e}")
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_routes.router)
 
 @app.get("/")
