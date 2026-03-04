68b51dff319e1b335223398a6b7bee47d486472b
ashen-v2 <ashen.te0217@gmail.com>
2026-03-04 18:36:12 +0530

    created users table and user input validation models

---
+++ backend/models/users.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timezone
+from sqlmodel import SQLModel, Field
+from pydantic import EmailStr
+
+class UserBase(SQLModel):
+    name: str = Field(max_length=100, nullable=False)
+    email: EmailStr = Field(max_length=100 , nullable=False, unique=True)
+
+class UserRead(UserBase):
+    address: str = Field(max_length=200, nullable=False)
+
+class UserCreate(UserRead):
+    password: str = Field(max_length=100, nullable=False)
+    
+class User(UserCreate, table=True):
+    id: int = Field(default=None, primary_key=True)
+    role: int = Field(default=1,nullable=False)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+
+
