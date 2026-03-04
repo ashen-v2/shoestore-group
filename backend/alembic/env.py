6eeb4bbda4763bd1dee4930cc6f7d615c45868b0
ashen-v2 <ashen.te0217@gmail.com>
2026-03-04 18:41:38 +0530

    created first alembic revision file and revised database

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 from config import settings
 from sqlmodel import SQLModel
+from models import users
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
