6e40a78666539b4b7e1c09f55a80a20f80aa5260
ashen-v2 <ashen.te0217@gmail.com>
2026-03-04 18:46:28 +0530

    add lifespan context manager for database connection handling added router to main

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,18 @@
 from fastapi import Depends, FastAPI
+from fastapi.concurrency import asynccontextmanager
+from db.session import engine, get_session
+from routes import user_routes
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        engine.connect()
+        print("Connection to PostgreSQL database successful!")
+    except Exception as e:
+        print(f"Error connecting to PostgreSQL database: {e}")
 
 app = FastAPI()
+app.include_router(user_routes.router)
 
 @app.get("/")
 async def root():
