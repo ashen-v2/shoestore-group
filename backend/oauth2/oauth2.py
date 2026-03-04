e4d4c9d5955dc59a8c4f4be0d45359675b1dadf9
ashen-v2 <ashen.te0217@gmail.com>
2026-03-04 19:27:28 +0530

    created jwt token encode, decode functions and get current user function to get current user

--- backend/oauth2/oauth2.py
+++ backend/oauth2/oauth2.py
@@ -1,11 +1,35 @@
+from fastapi import Depends, HTTPException
 import jwt
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt.exceptions import InvalidTokenError
 from config import settings
 
-SECRET_KEY = settings.secret_key
-ALGORITHEM = settings.algorithm
-ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+SECRET_KEY : str  = settings.secret_key
+ALGORITHEM : str = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES : int = settings.access_token_expire_minutes
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
+def create_scess_token(data : dict):
+    """Create a JWT access token."""
+    to_encode : dict = data.copy()
+    encoded_jwt : str = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHEM)
+    return encoded_jwt
 
-
+def verify_access_token(token: str):
+    """Verify the JWT access token and return the payload"""
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHEM])
+        return payload
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token") from e
+    
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    """Get the current user from the JWT access token."""
+    try:
+        payload = verify_access_token(token)
+        user_id : str = payload.get("user_id")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return user_id
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token")
