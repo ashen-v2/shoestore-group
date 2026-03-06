e31fff359abf03a995940810b1c83d89841e5d53
ashen-v2 <ashen.te0217@gmail.com>
2026-03-06 09:16:17 +0530

    add order and orderitem models with schema definitions and relationships

--- backend/models/orders.py
+++ backend/models/orders.py
@@ -19,3 +19,17 @@ class CartItemRead(SQLModel):
     cart_id : int
     stock_id : int
     quantity : int
+
+class Order(SQLModel, table=True):
+    id : int = Field(primary_key=True, nullable=False)
+    user_id : int = Field(foreign_key="user.id", nullable=False, ondelete="CASCADE")
+    total_price : float = Field(default=0.0, nullable=False)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
+
+class OrderItem(SQLModel, table=True):
+    id : int = Field(primary_key=True, nullable=False)
+    order_id : int = Field(foreign_key="order.id", nullable=False, ondelete="CASCADE")
+    stock_id : int = Field(foreign_key="stock.id", nullable=False, ondelete="CASCADE")
+    quantity : int = Field(default=1, nullable=False)
+    price_locked : float = Field(default=0.0, nullable=False)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
