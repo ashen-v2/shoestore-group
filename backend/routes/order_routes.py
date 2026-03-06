aa84c8697b6faa0812fbf8bd066aa988604cdf98
ashen-v2 <ashen.te0217@gmail.com>
2026-03-06 09:56:24 +0530

    add delivery and payment status to Order model; update OrderItem with price_locked; create orders table revision

--- backend/routes/order_routes.py
+++ backend/routes/order_routes.py
@@ -3,7 +3,7 @@ from sqlmodel import Session, select
 from models.tokens import TokenData
 from dependancies.dependancies import get_current_user
 from db.session import get_session
-from models.orders import Order, OrderItem
+from models.orders import Order, OrderItem, OrderItemRead
 
 
 router : APIRouter = APIRouter( prefix="/orders", tags=["orders"])
@@ -13,3 +13,20 @@ def get_orders(session : Session = Depends(get_session), current_user : TokenDat
     """Get the current user's orders"""
     db_orders = session.exec(select(Order).where(Order.user_id == current_user.user_id).offset(skip).limit(limit)).all()
     return db_orders
+
+@router.get("/{order_id}", status_code=200)
+def get_order(order_id : int, session : Session = Depends(get_session), current_user : TokenData = Depends(get_current_user)):
+    """Get an order by ID"""
+    order = session.get(Order, order_id)
+    if not order or order.user_id != current_user.user_id:
+        raise HTTPException(status_code=404, detail="Order not found")
+    if order.user_id != current_user.user_id:
+        raise HTTPException(status_code=403, detail="Not authorized to access this order")
+    order_items = session.exec(select(OrderItem).where(OrderItem.order_id == order_id)).all()
+    return order, order_items
+
+@router.post("/", status_code=200)
+def create_order(order_id : int, session : Session = Depends(get_session), current_user : TokenData = Depends(get_current_user)):
+    """Create an order"""
+    pass
+    
