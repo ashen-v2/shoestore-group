becf4005d2e7ba1b0238faa5fc12cc781872f9ec
ashen-v2 <ashen.te0217@gmail.com>
2026-03-08 18:22:15 +0530

    Add route to fetch all issue tickets for the current user. limit 20 per request

--- backend/routes/issueticket_routes.py
+++ backend/routes/issueticket_routes.py
@@ -8,7 +8,15 @@ from models.tokens import TokenData
 
 router : APIRouter = APIRouter( prefix="/issuetickets", tags=["issuetickets"])
 
-@router.post("/{order_id}", status_code=201)
+@router.get("/", response_model=list[IssueTicket], status_code=200)
+def get_issue_tickets(session : Session = Depends(get_session),
+                       current_user : TokenData = Depends(get_current_user), limit : int = 20, offset : int = 0 ):
+    """Get all issue tickets for the current user"""
+    issue_tickets : list[IssueTicket] = session.exec(select(IssueTicket)
+                                                     .where(IssueTicket.user_id == current_user.user_id).offset(offset).limit(limit)).all()
+    return issue_tickets
+
+@router.post("/{order_id}", status_code=201, response_model=IssueTicket)
 def create_issue_ticket(order_id : int, issueticket: IssueTicketCreate, session : Session = Depends(get_session),
                          current_user : TokenData = Depends(get_current_user)):
     """Create an issue ticket for an order"""
